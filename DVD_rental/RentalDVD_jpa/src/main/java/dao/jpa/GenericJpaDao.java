100aec62e8570c90844a759250ba545ebf4ea6c1
Mateusz Samluk <mazsak97@gmail.com>
2018-06-17 18:27:18 +0200

    Prawie bez zmian

---
+++ DVD_rental/RentalDVD_jpa/src/main/java/dao/jpa/GenericJpaDao.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao.jpa;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import dao.GenericDao;
+import javax.persistence.EntityManager;
+import util.JpaFactory;
+
+/**
+ *
+ * @author student
+ */
+public class GenericJpaDao<T, K> implements GenericDao<T, K> {
+
+    private final Class<T> type;
+
+    public GenericJpaDao() {
+        Type t = getClass().getGenericSuperclass();
+        ParameterizedType pt = (ParameterizedType) t;
+        type = (Class) pt.getActualTypeArguments()[0];
+    }
+
+    @Override
+    public void save(T t) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.persist(t);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void delete(T t) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        t = em.merge(t);
+        em.remove(t);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void update(T t) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.merge(t);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public T findById(K id) {
+        EntityManager em = getEntityManager();
+        T dto = em.find(type, id);
+        em.close();
+        return dto;
+    }
+
+    protected EntityManager getEntityManager() {
+        return JpaFactory.getEntityManager();
+    }
+}
